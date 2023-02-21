dcf6b4c0ab380f6aa509d1aedce7d3b98951bd22
Antonio010R <antonroy009@gmail.com>
2023-02-21 21:49:10 +0530

    Completed firebase setup

--- src/redux/sagas/coins.saga.js
+++ src/redux/sagas/coins.saga.js
@@ -2,8 +2,7 @@ import { takeLatest, call, all, put } from "redux-saga/effects";
 import axios from "axios";
 
 export function* fetchCoinList() {
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+  const url = process.env.REACT_APP_COIN_LIST_API;
 
   try {
     const request = yield call(axios.get, url);
@@ -18,7 +17,7 @@ export function* fetchCoinList() {
 export function* fetchCoinDetails({ payload }) {
   // console.log(payload);
   const url =
-    yield `https://api.coingecko.com/api/v3/coins/${payload}?localization=false&sparkline=true`;
+    yield `${process.env.REACT_APP_COIN_DETAILS_API}/${payload}?localization=false&sparkline=true`;
   try {
     const request = yield call(axios.get, url);
     const data = yield request.data;
